c4f1326cedc30c82243faa042bee3bb163fdbf14
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-04-26 00:32:38 +0200

    add getAll-request (frontend & backend)

--- backend/src/main/java/com/github/astridottmann/backend/controllers/RouteController.java
+++ backend/src/main/java/com/github/astridottmann/backend/controllers/RouteController.java
@@ -5,11 +5,18 @@ import com.github.astridottmann.backend.services.RouteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/routes")
 @RequiredArgsConstructor
 public class RouteController {
     private final RouteService routeService;
+
+    @GetMapping
+    public List<Route> getAllRoutes(){
+       return routeService.getAllRoutes();
+    }
 
     @PostMapping
     public Route addRoute(@RequestBody Route route) {
