ab8aab6f19f5a2144bb0ea3a3490aaa827b9a6eb
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-04-25 17:41:45 +0200

    remove code smells

--- backend/src/main/java/com/github/astridottmann/backend/controllers/RouteController.java
+++ backend/src/main/java/com/github/astridottmann/backend/controllers/RouteController.java
@@ -1,8 +1,6 @@
 package com.github.astridottmann.backend.controllers;
 
-import com.github.astridottmann.backend.models.Car;
 import com.github.astridottmann.backend.models.Route;
-import com.github.astridottmann.backend.models.Vehicle;
 import com.github.astridottmann.backend.services.RouteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class RouteController {
     private final RouteService routeService;
 
-   /* @GetMapping
-    public Route getRoute(){
-        Vehicle vehicle = new Car( 0.9f, "diesel", "large");
-        Route newRoute = new Route("test", "MA", "SP",
-                25.0f, 1, false, vehicle, 0.0f);
-        return newRoute;
-    }*/
     @PostMapping
     public Route addRoute(@RequestBody Route route) {
         return routeService.addRoute(route);
