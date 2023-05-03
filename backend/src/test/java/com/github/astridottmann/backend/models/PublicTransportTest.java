bdf6731d4f5a9260387376e24c8ac2752981ba91
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-05-03 03:41:03 +0200

    add car- & publicTransport-service-tests (getCo2Emission)

--- backend/src/test/java/com/github/astridottmann/backend/models/PublicTransportTest.java
+++ backend/src/test/java/com/github/astridottmann/backend/models/PublicTransportTest.java
@@ -20,4 +20,14 @@ class PublicTransportTest {
         assertEquals("local", publicTransport.getDistanceLevel());
         assertEquals("train", publicTransport.getMeansOfTransport());
     }
+
+    @Test
+    void getCo2Emission() {
+        Vehicle vehicle = new PublicTransport("PublicTransport", 108.0, "local","bus");
+
+        double expectedCo2Emission = 108.0;
+
+        double actualCo2Emission = vehicle.getCo2Emission();
+        assertEquals(expectedCo2Emission, actualCo2Emission, 0.01);
+    }
 }
