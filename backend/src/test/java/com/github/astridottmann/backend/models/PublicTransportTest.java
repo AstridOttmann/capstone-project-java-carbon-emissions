c5bb06fa397cd2f674c14fd1745fc09427250df6
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-05-03 03:50:58 +0200

    small changes

--- backend/src/test/java/com/github/astridottmann/backend/models/PublicTransportTest.java
+++ backend/src/test/java/com/github/astridottmann/backend/models/PublicTransportTest.java
@@ -23,9 +23,9 @@ class PublicTransportTest {
 
     @Test
     void getCo2Emission() {
-        Vehicle vehicle = new PublicTransport("PublicTransport", 108.0, "local","bus");
+        Vehicle vehicle = new PublicTransport("PublicTransport", 37.0, "longDistance","bus");
 
-        double expectedCo2Emission = 108.0;
+        double expectedCo2Emission = 37.0;
 
         double actualCo2Emission = vehicle.getCo2Emission();
         assertEquals(expectedCo2Emission, actualCo2Emission, 0.01);
