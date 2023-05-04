251cb81185b0dea80268834b16ea397332c2f9e5
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-05-04 21:36:29 +0200

    fix display bug

---
+++ frontend/src/components/CompareRoutesResults.tsx
@@ -0,0 +1,23 @@
+import {Box, Typography} from "@mui/material";
+import {Route} from "../models/RouteModel";
+
+type CompareRoutesResultsProps = {
+    route: Route
+}
+export default function CompareRoutesResults(props: CompareRoutesResultsProps) {
+    return (
+      /*  <Box sx={{display: "flex", gap: "1rem",}}>*/
+            <Box  sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                borderRadius: 1,
+                width: "50%",
+                backgroundColor: "#3fd44d"
+            }}>
+                <Typography sx={{textAlign: "center"}}>{props.route.vehicle.type}</Typography>
+                <Typography sx={{textAlign: "center"}}>{props.route.co2EmissionRoute}</Typography>
+            </Box>
+     /*   </Box>*/
+    )
+}
