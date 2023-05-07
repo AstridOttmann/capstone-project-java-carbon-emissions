119844edad367869aeece8eb713dc973ac6f1faf
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-05-07 21:35:34 +0200

    remove @Document annotation

--- backend/src/main/java/com/github/astridottmann/backend/models/CompareRoutes.java
+++ backend/src/main/java/com/github/astridottmann/backend/models/CompareRoutes.java
@@ -1,11 +1,10 @@
 package com.github.astridottmann.backend.models;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document("compare-routes")
+
 public record CompareRoutes(
         @Id
         String id,
