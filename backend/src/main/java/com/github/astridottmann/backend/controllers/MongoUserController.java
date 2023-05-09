5d7b3d231aa3d379bd4b1aed16416c78020a1080
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-05-09 11:17:00 +0200

    implement password encoder

--- backend/src/main/java/com/github/astridottmann/backend/controllers/MongoUserController.java
+++ backend/src/main/java/com/github/astridottmann/backend/controllers/MongoUserController.java
@@ -1,5 +1,6 @@
 package com.github.astridottmann.backend.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class MongoUserController {
-    @GetMapping
-    public String getUsername() {
-        return "test";
+
+    @GetMapping("/me")
+    public String getMe() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
     }
 }
